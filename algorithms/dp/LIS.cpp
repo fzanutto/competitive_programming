e62cab95a27f434110904f91e00395c23f70ecbc
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-09-12 09:46:05 -0300

    Update LIS.cpp

--- algorithms/dp/LIS.cpp
+++ algorithms/dp/LIS.cpp
@@ -1,33 +1,56 @@
-/* Dynamic Programming C++ implementation of LIS problem */
-#include<bits/stdc++.h>  
-using namespace std; 
-    
-/* lis() returns the length of the longest increasing  
-  subsequence in arr[] of size n */
-int lis( int arr[], int n )  
-{  
-    int lis[n]; 
-   
-    lis[0] = 1;    
+#include <iostream> 
+#include <vector> 
   
-    /* Compute optimized LIS values in bottom up manner */
-    for (int i = 1; i < n; i++ )  
-    { 
-        lis[i] = 1; 
-        for (int j = 0; j < i; j++ )   
-            if ( arr[i] > arr[j] && lis[i] < lis[j] + 1)  
-                lis[i] = lis[j] + 1;  
+// Binary search (note boundaries in the caller) 
+int CeilIndex(std::vector<int>& v, int l, int r, int key) 
+{ 
+    while (r - l > 1) { 
+        int m = l + (r - l) / 2; 
+        if (v[m] >= key) 
+            r = m; 
+        else
+            l = m; 
     } 
   
-    // Return maximum value in lis[] 
-    return *max_element(lis, lis+n); 
-}  
-    
-/* Driver program to test above function */
-int main()  
-{  
-    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };  
-    int n = sizeof(arr)/sizeof(arr[0]);  
-    printf("Length of lis is %d\n", lis( arr, n ) );  
-    return 0;  
-}
+    return r; 
+} 
+  
+int LongestIncreasingSubsequenceLength(std::vector<int>& v) 
+{ 
+    if (v.size() == 0) 
+        return 0; 
+  
+    std::vector<int> tail(v.size(), 0); 
+    int length = 1; // always points empty slot in tail 
+  
+    tail[0] = v[0]; 
+    for (size_t i = 1; i < v.size(); i++) { 
+  
+        // new smallest value 
+        if (v[i] < tail[0]) 
+            tail[0] = v[i]; 
+  
+        // v[i] extends largest subsequence 
+        else if (v[i] > tail[length - 1]) 
+            tail[length++] = v[i]; 
+  
+        // v[i] will become end candidate of an existing 
+        // subsequence or Throw away larger elements in all 
+        // LIS, to make room for upcoming grater elements 
+        // than v[i] (and also, v[i] would have already 
+        // appeared in one of LIS, identify the location 
+        // and replace it) 
+        else
+            tail[CeilIndex(tail, -1, length - 1, v[i])] = v[i]; 
+    } 
+  
+    return length; 
+} 
+  
+int main() 
+{ 
+    std::vector<int> v{ 2, 5, 3, 7, 11, 8, 10, 13, 6 }; 
+    std::cout << "Length of Longest Increasing Subsequence is "
+              << LongestIncreasingSubsequenceLength(v) << '\n'; 
+    return 0; 
+} 
