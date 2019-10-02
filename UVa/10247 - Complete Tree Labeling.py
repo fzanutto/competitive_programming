0df51e6c218de3ba7507908de17c5511996de24f
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-10-02 17:28:18 -0300

    Added UVa Complete Tree Labeling

---
+++ UVa/10247 - Complete Tree Labeling.py
@@ -0,0 +1,30 @@
+# Alunos: Fernando Zanutto e Guilherme Santana
+# ID user UVa: 1046965
+# ID submission: 23991425
+
+
+from math import factorial
+def n(k,d):
+
+    return (k**(d+1) - 1) / (k-1)
+    
+    
+def f(k,d):
+    if d == 0: 
+        return 1
+    if d == 1:
+        return factorial(k)
+    if k == 1:
+        return 1
+        
+    return factorial(n(k,d)-1) * f(k, d-1)**k // factorial(n(k,d-1))**k 
+    
+    
+    
+while True:
+    try:
+        a,b = [int(i) for i in input().split()]
+        
+        print(f(a,b))
+    except EOFError:
+        break
